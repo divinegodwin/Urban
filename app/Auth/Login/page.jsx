6870a0a9deb33b0afffb6729c54e25f744c191ed
Agbasimelo Divine <divinegodwin172@gmail.com>
2024-08-07 10:24:05 -0700

    signin

--- app/Auth/Login/page.jsx
+++ app/Auth/Login/page.jsx
@@ -4,9 +4,8 @@ import supabase from "@/app/supabaseClient";
 import Loader from "@/app/Loader";
 import Link from "next/link";
 
-const Signup = () => {
+const Login = () => {
   const [formData, setFormData] = useState({
-    username: "",
     email: "",
     password: "",
   });
@@ -19,37 +18,26 @@ const Signup = () => {
       [e.target.name]: e.target.value,
     }));
   }
-  const handleSignup = async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    const { username, email, password } = formData;
-    if ((username || email || password) == "") {
+    const { email, password } = formData;
+    if ((email || password) == "") {
       setError("all input fields must be filled");
-      return;
-    }
-    let passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
-    if (!passwordRegex.test(password) && password.length < 8) {
-      setError("use a strong password");
-      return;
-    }
-    var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (!emailRegex.test(email)) {
-      setError("use a valid email");
       return;
     }
     try {
       setLoading(true)
-      const { data, error } = await supabase.auth.signUp({
-        email: formData.email,
-        password: formData.password,
-        options: {
-          data: {
-            user_name: formData.username,
-          },
-        },
-      });
-      alert("check your email for verification");
+      
+const { data, error } = await supabase.auth.signInWithPassword({
+    email: formData.email,
+    password: formData.password                ,
+  })
+  if(error){
+    setError('No account found')
+  }
+    
     } catch (error) {
+        setError('You do not have an account, please create one')
       console.log(error);
     }finally{
       setError('')
@@ -61,18 +49,9 @@ const Signup = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-2xl font-bold mb-6 text-center">SignUp</h2>
-        {error && <p className="text-red-500">{error}</p>}
-        <div className="mb-4">
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={formData.username}
-            onChange={handleChange}
-            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-[#1746c3]"
-          />
-        </div>
+        <h2 className="text-2xl font-bold mb-6 text-center">SignIn</h2>
+        {error && <p className="text-red-500 text-lg">{error}</p>}
+      
         <div className="mb-4">
           <input
             type="email"
@@ -95,16 +74,16 @@ const Signup = () => {
           />
         </div>
         <button
-          onClick={handleSignup}
+          onClick={handleLogin}
           className="w-full bg-[#1746c3] text-white py-3 rounded-lg hover:bg-blue-500 transition-colors"
         >
-          Sign Up
+          Sign In
         </button>
-        
-        <p className="text-center pt-4">Already have an account?<Link href='/Auth/Login'> <span className="text-[#1746c3]">Sign In</span></Link></p>
+        <p className="text-center pt-4">Don't have an account?<Link href='/Auth/Signup'> <span className="text-[#1746c3]">SignUp</span></Link></p>
+
       </div>
     </div>
   );
 };
 
-export default Signup;
+export default Login;
