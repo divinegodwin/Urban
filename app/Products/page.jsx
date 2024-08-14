040143d3d0c88f1b555739eff65775db3df3869e
Agbasimelo Divine <divinegodwin172@gmail.com>
2024-08-15 00:43:13 +0100

    quantity option

--- app/Products/page.jsx
+++ app/Products/page.jsx
@@ -42,6 +42,19 @@ const Products = () => {
     setModalIsClosed(false);
   };
 
+  const [orderQuantity, setOrderQuantity] = useState(0)
+  const addQuantity = () =>{
+      setOrderQuantity((previousQuantity)=> previousQuantity+1)
+  }
+
+ 
+  const removeQuantity = () =>{
+    if(orderQuantity > 0){
+    setOrderQuantity((previousQuantity)=> previousQuantity-1)
+    }
+    }
+
+
   return (
     <div>
       <Navbar />
@@ -96,13 +109,15 @@ const Products = () => {
       )}
 
       {selectedProduct && (
-       <div
-       className={`fixed inset-0 flex justify-center bg-[#000] bg-opacity-90 p-4 ${ modalIsClosed ? "hidden" : "drop-modal"}`}
-     >
+        <div
+          className={`fixed  overflow-hidden overflow-y-auto inset-0 flex justify-center bg-[#000] bg-opacity-90 p-14 ${
+            modalIsClosed ? "hidden" : "drop-modal"
+          }`}
+        >
           <div key={selectedProduct.id} className="text-[#ffff]">
             <svg
               onClick={closeModal}
-              className="w-[30px] float-right mr-15 mt-6"
+              className="w-[30px] float-right mr-15 mt-6 mb-4"
               data-slot="icon"
               fill="none"
               strokeWidth="1.5"
@@ -118,19 +133,81 @@ const Products = () => {
               ></path>
             </svg>
 
+            <svg
+              className="w-[30px] mt-7"
+              data-slot="icon"
+              fill="none"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+              ></path>
+            </svg>
+
             <img src={selectedProduct.product_image}></img>
-            <p className="font-bold text-3xl ">
-              {selectedProduct.product_name}
-            </p>
-            <p className="text-lg">{selectedProduct.product_description}</p>
-            <p className="font-mono text-lg">
-              {selectedProduct.currency}
-              {selectedProduct.product_price}
-            </p>
-            <button className="w-[180px] h-[50px] bg-slate-200 rounded-full mt-4 border-none text-[#000]">
-              {" "}
-              Add to Cart
-            </button>
+
+            <div className="text-center">
+              <p className="font-bold text-3xl pb-2 ">
+                {selectedProduct.product_name}
+              </p>
+              <p className="text-lg pb-2">
+                {selectedProduct.product_description}
+              </p>
+              <p className="font-mono text-2xl font-bold">
+                {selectedProduct.currency}
+                {selectedProduct.product_price}
+              </p>
+            </div>
+            <div className="flex gap-3 mt-2 justify-center">
+              <p className="text-lg">Quantity</p>
+              <svg
+               onClick={removeQuantity}
+               className="w-[30px]"
+                data-slot="icon"
+                fill="none"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                ></path>
+              </svg>
+              <p className="text-lg">{orderQuantity}</p>
+              <svg
+              onClick={addQuantity}
+              className="w-[30px] ml-1"
+                data-slot="icon"
+                fill="none"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                ></path>
+              </svg>
+            </div>
+            <div className="flex justify-center mb-5">
+              <button className=" w-[180px] h-[50px] bg-slate-200 rounded-full mt-4 border-none text-[#000]">
+                {" "}
+                Add to Cart
+              </button>
+            </div>
           </div>
         </div>
       )}
